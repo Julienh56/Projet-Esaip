564da4d885b374eb6dbb6ab285a84b03e797b4c5
Julienh56 <haasjulien@gmail.com>
2017-02-01 16:30:14 +0100

    Push: Mise en place du projet dans le repository

---
+++ projetesaip/demo/src/main/java/projetesaip/HomeController.java
@@ -0,0 +1,22 @@
+package projetesaip;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = {"/", "/home", "/welcome"})
+public class HomeController {
+  
+ 
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String home() {
+  
+    return "home";
+  }
+}
+	
+	
+
